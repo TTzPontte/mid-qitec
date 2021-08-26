af76bbe78db9a8f3f2da255e60df8f795d954886
lucas souza <ibmstemp@gmail.com>
2021-08-26 13:01:23 -0300

    fix: comment google module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { DebtsModule } from "./debts/debts.module";
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { EscrowModule } from "./escrow/escrow.module";
-import { GoogleDriveModule } from "./google-drive/google-drive.module";
+// import { GoogleDriveModule } from "./google-drive/google-drive.module";
 import { PontteContractModule } from "./pontte-contract/pontte-contract.module";
 import { QitechModule } from "./qitech/qitech.module";
 
--- src/debts/dto/RePossessionComposition.dto.ts
+++ src/debts/dto/RePossessionComposition.dto.ts
@@ -1,8 +1,5 @@
-import { RealEstateDto } from "./RealEstate.dto";
-
 export class RePossessionCompositionDto {
   id: number;
   name: string | null;
   percentage: string | null;
-  // realEstates: RealEstateDto;
 }
