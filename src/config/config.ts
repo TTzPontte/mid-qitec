2979e4296d8346616a572ecc693e0b50ebd6cd29
Oliver Osti <oliverosti@gmail.com>
2021-12-15 13:46:02 -0300

    Arquivo de configuração com o caminho dos arquivos pegando dentro do projeto.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,10 @@
 const config = {
     GOOGLE_SCOPE : 'https://www.googleapis.com/auth/drive',
     GOOGLE_DRIVE_FOLDER:'/opt',
-    GOOGLE_CREDENTIAL_RAW_DATA:'/opt/pontte/arquivos/vegapontte-c37fda9c7b54.json',
+    GOOGLE_CREDENTIAL_RAW_DATA: __dirname.concat('vegapontte-c37fda9c7b54.json'),
     QITECH_CLIENTKEY: "3fb54232-00c3-44c1-bd99-883f3c50da46",
-    QITECH_PRIVATEKEY: "/opt/pontte/arquivos/jwtECDSASHA512.key",
-    QITECH_PUBLICKEY: "/opt/pontte/arquivos/jwtECDSASHA512.key.pub",
+    QITECH_PRIVATEKEY: __dirname.concat("jwtECDSASHA512.key"),
+    QITECH_PUBLICKEY: __dirname.concat("jwtECDSASHA512.key.pub"),
     ROOT_FOLDER_ID: "12prrnSQu4UDoxSaTzMMxkSchUWDJ-5H4",
 }
 
